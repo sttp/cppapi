d82dc9425cbe4ac5371f824c69cf30e1fd137213
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-07-03 15:42:54 -0400

    Updated to version 1.0.10

--- src/lib/Version.h
+++ src/lib/Version.h
@@ -25,7 +25,7 @@
 #define __VERSION_H
 
 #define STTP_TITLE "STTP C++ Library"
-#define STTP_VERSION "1.0.9"
-#define STTP_UPDATEDON "2019-07-02"
+#define STTP_VERSION "1.0.10"
+#define STTP_UPDATEDON "2019-07-03"
 
 #endif
